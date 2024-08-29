393f6cc82cc1f8b0f5dd3ca06b91ac0e4e25594e
Podolianyk <podolianik1988@gmail.com>
2024-08-29 11:30:58 +0200

    create component html structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,20 @@
+import { useState } from 'react';
 import DailyNormaModal from './DailyNormaModal/DailyNormaModal';
 
 const App = () => {
+  const [modalState, setModalState] = useState(false);
+  const openModal = () => {
+    setModalState(true);
+  };
+  const closeModal = () => {
+    setModalState(false);
+  };
+
   return (
     <div>
       <p>'React homework template'</p>
-      <button>Save</button>
-      <DailyNormaModal />
+      <button onClick={openModal}>Open</button>
+      <DailyNormaModal isOpen={modalState} closeModal={closeModal} />
     </div>
   );
 };
--- src/components/DailyNormaModal/DailyNormaModal.jsx
+++ src/components/DailyNormaModal/DailyNormaModal.jsx
@@ -1,8 +1,66 @@
 import style from './DailyNormaModal.module.css';
 import { useState } from 'react';
 
-const DailyNormaModal = ({}) => {
-  return <div>DailyNormaModal</div>;
+const DailyNormaModal = ({ isOpen, closeModal }) => {
+  if (!isOpen) {
+    return;
+  }
+  return (
+    <div className={style.backdrop}>
+      <div className={style.modal}>
+        <div>
+          <h3>My daily norma</h3>
+          <button onClick={closeModal}>Close</button>
+        </div>
+        <div>
+          <div>
+            <div>
+              <p>
+                For girl:<span>V=(M*0,03) + (T*0,4)</span>
+              </p>
+              <p>
+                For man:<span>V=(M*0,04) + (T*0,6)</span>
+              </p>
+            </div>
+            <p>
+              * V is the volume of the water norm in liters per day, M is your
+              body weight, T is the time of active sports, or another type of
+              activity commensurate in terms of loads (in the absence of these,
+              you must set 0)
+            </p>
+          </div>
+          <div>
+            <h4>Calculate your rate:</h4>
+            <div>
+              <div>
+                <input type="radio" />
+                <label>For woman</label>
+              </div>
+              <div>
+                <input type="radio" />
+                <label>For man</label>
+              </div>
+            </div>
+            <p>Your weight in kilograms:</p>
+            <input type="text" name="" placeholder="0" />
+            <p>
+              The time of active participation in sports or other activities
+              with a high physical. load in hours:
+            </p>
+            <input type="text" name="" placeholder="0" />
+            <p>
+              The required amount of water in liters per day:<span>1.8 L</span>
+            </p>
+            <p>Write down how much water you will drink:</p>
+            <input type="text" name="" placeholder="0" />
+          </div>
+        </div>
+        <div>
+          <button onClick={closeModal}>Save</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default DailyNormaModal;
