58e8ebaedd119403a4e22cd44deb3e404cd937e4
ilonivna <147661751+ilonivna@users.noreply.github.com>
2024-08-29 14:17:01 -0400

    setup MongoDB

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,45 +1,9 @@
-import express from 'express';
-import pino from 'pino-http';
-import cors from 'cors';
+import { startServer } from "./server.js";
+import { initMongoDB } from "./db/initMongoDB.js";
 
+const bootstrap = async () => {
+  await initMongoDB();
+  startServer();
+};
 
-const app = express();
-
-const PORT = 3000;
-
-app.use(express.json());
-app.use(cors());
-app.use(
-  pino({
-    transport: {
-      target: 'pino-pretty',
-    },
-  }),
-);
-
-app.get('/', (req, res) => {
-  res.json({
-    message: 'Hello world'
-  });
-});
-
-app.use('*', (req, res, next) => {
-  res.status(404).json({
-    message: 'Route not found',
-  });
-});
-
-app.use((err, req, res, next) => {
-  res.status(500).json({
-    message: 'Something went wrong',
-    error: err.message,
-  });
-});
-
-
-
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
-
+bootstrap();
