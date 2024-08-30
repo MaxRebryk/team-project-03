2b5f40ac40442c38b35dffb92fa8b86417505a6a
OlyaIarova <domaschuk.olya@gmail.com>
2024-08-30 08:00:03 +0300

    Fixroutes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
+import usersRouter from './users.js';
 import waterRouter from './water.js';
 
 const router = Router();
 
+router.use('/users', usersRouter);
 router.use('/water', waterRouter);
-// router.use('/auth', authRouter);
+
 
 export default router;
