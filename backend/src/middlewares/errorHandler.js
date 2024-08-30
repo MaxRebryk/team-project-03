584c74c189dd6bcbbf4c60d7dcab2f7ace2caad6
Aleksan Vartapetyan <alex.240941.123@gmail.com>
2024-08-30 13:25:34 +0900

    update js files

--- backend/src/middlewares/errorHandler.js
+++ backend/src/middlewares/errorHandler.js
@@ -1,9 +1,6 @@
 import { HttpError } from 'http-errors';
 
 export const errorHandler = (err, req, res, next) => {
-
-
-
   if (err instanceof HttpError) {
     res.status(err.status).json({
       status: err.status,
@@ -13,11 +10,11 @@ export const errorHandler = (err, req, res, next) => {
     return;
   }
 
-
   res.status(500).json({
     status: 500,
     message: 'Something went wrong',
     data: err.message,
+  });
 
   res.status(500).json({
     status: 500,
