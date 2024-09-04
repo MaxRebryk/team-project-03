194ca22057fa7865db3b998bc593f9ae640dbef5
Andrii <andriybilous121392@gmail.com>
2024-09-03 21:02:03 -0500

    Heder+auth/logOut

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,3 +1,12 @@
+// import axios from 'axios';
+// import { createAsyncThunk } from '@reduxjs/toolkit';
+
+// const setAuthHeader = token => {
+//   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
+// };
+// const clearAuthHeader = () => {
+//   axios.defaults.headers.common.Authorization = '';
+// };
 // export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
 //   try {
 //     await axios.post('/users/logout');
