3ca91a5e28f602e39e7e2b1b8eeca7f20ffe830a
OlyaIarova <domaschuk.olya@gmail.com>
2024-09-05 20:44:54 +0300

    update

--- backend/src/utils/parseFilterParams.js
+++
@@ -1,8 +0,0 @@
-export const parseFilterParams = query => {
-  const { day, month } = query;
-
-  return {
-    day: day ? parseInt(day) : undefined,
-    month: month ? month : undefined,
-  };
-};
