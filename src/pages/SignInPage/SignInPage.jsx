8fd500d242ca225fe2aeb3836e55f13e366f6a30
AnastasiaNazar82 <148636770+AnastasiaNazar82@users.noreply.github.com>
2024-09-05 19:49:18 +0300

    added changes to signin

--- src/pages/SignInPage/SignInPage.jsx
+++ src/pages/SignInPage/SignInPage.jsx
@@ -1,11 +1,24 @@
-// import { useSelector } from 'react-redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import LoginForm from 'components/LoginForm/LoginForm.jsx';
-// import { selectIsLoading } from 'redux/auth/selectors';
+import { selectIsLoading } from 'redux/auth/auth.selectors.js';
+import Loader from 'components/Loader/Loader.jsx';
+import style from './SignInPage.module.css';
 
 const SignInPage = () => {
+  const isLoader = useSelector(selectIsLoading);
+
   return (
-    <div>
-      <LoginForm />
+    <div className={style.container}>
+      <div className={style.background}>
+        {isLoader ? (
+          <Loader />
+        ) : (
+          <>
+            <LoginForm />
+          </>
+        )}
+      </div>
     </div>
   );
 };
